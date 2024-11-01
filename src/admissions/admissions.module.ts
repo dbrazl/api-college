209f873f94cfecc5569a3ccb0d219d766d8406ac
Daniel Braz <danielbrazlopes16@gmail.com>
2024-11-01 03:28:46 -0300

    feat: inject student in admissions

--- src/admissions/admissions.module.ts
+++ src/admissions/admissions.module.ts
@@ -3,6 +3,7 @@ import { AdmissionsService } from './admissions.service';
 import { AdmissionsController } from './admissions.controller';
 import { ADMISSION_SERVICE_INTERFACE } from 'src/injection.interface.types';
 import { PrismaService } from 'src/admissions/prisma.service';
+import { Student } from 'src/business/student.entity';
 
 @Module({
   providers: [
@@ -11,6 +12,7 @@ import { PrismaService } from 'src/admissions/prisma.service';
       useClass: AdmissionsService,
     },
     PrismaService,
+    Student,
   ],
   controllers: [AdmissionsController],
 })
