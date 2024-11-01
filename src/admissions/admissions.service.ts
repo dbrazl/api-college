92f81bb5d1b389b91f95fb321a0285a3f473a9eb
Daniel Braz <danielbrazlopes16@gmail.com>
2024-11-01 03:53:43 -0300

    feat: map students

--- src/admissions/admissions.service.ts
+++ src/admissions/admissions.service.ts
@@ -1,8 +1,9 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { IAdmissionsService } from './interfaces/admission.service.interface';
 import { Admission } from './@types/Admission';
+import { Student as StudentType } from './@types/Student';
 import { PrismaService } from 'src/admissions/prisma.service';
-import { Student } from './@types/Student';
+import { Student } from '../business/student.entity';
 
 @Injectable()
 export class AdmissionsService implements IAdmissionsService {
@@ -10,8 +11,17 @@ export class AdmissionsService implements IAdmissionsService {
   private readonly prismaService: PrismaService;
 
   public async index(): Promise<Admission[]> {
-    const students = await this.prismaService.students.findMany();
+    const studentsPersisted: StudentType[] =
+      await this.prismaService.students.findMany();
+    const students: Student[] = this.mapStudents(studentsPersisted);
     return this.mapToAdmissions(students);
+  }
+
+  private mapStudents(students: StudentType[]): Student[] {
+    return students.map(
+      ({ id, uuid, name, email, score }: StudentType) =>
+        new Student(id, uuid, name, email, score),
+    );
   }
 
   private mapToAdmissions(students: Student[]): Admission[] {
@@ -20,7 +30,7 @@ export class AdmissionsService implements IAdmissionsService {
       name: student.name,
       email: student.email,
       score: student.score,
-      admitted: true,
+      admitted: student.isAdmitted(),
     }));
   }
 }
